f4c65f60aa8a436322768cdc9610e22afd3b6059
Shaurya Chauhan <blues9524@gmail.com>
2026-07-14 03:46:03 +0530

    feat(web): add shared icon exports

---
+++ apps/web/src/components/ui/Icons/index.ts
@@ -0,0 +1,28 @@
+// general
+export {
+  LuUserRound as UserIcon,
+  LuMessageCircle as ChatIcon,
+  LuVideo as VideoIcon,
+  LuPencilRuler as WhiteboardIcon,
+  LuStickyNote as StickyNoteIcon,
+  LuFileText as DocIcon,
+  LuCopyright as CopyrightIcon
+} from "react-icons/lu"
+
+export {
+  CiLight as LightModeIcon,
+  CiDark as DarkModeIcon
+} from "react-icons/ci"
+
+export {
+  FaArrowRightLong as RightArrowIcon
+} from "react-icons/fa6"
+
+// toast
+export {
+  LuCircleAlert as ErrorIcon,
+  LuTriangleAlert as WarningIcon,
+  LuCircleCheck as SuccessIcon,
+  LuInfo as InfoIcon,
+  LuX as CloseIcon
+} from "react-icons/lu"
